d1478dbae9119e96b238cb0ebc4fddb52adac142
Anthony <atsui4688@gmail.com>
2020-04-14 16:09:41 -0400

    Adding HTML transformer

--- index.js
+++ index.js
@@ -15,32 +15,67 @@ addEventListener('fetch', event => {
 //   return Response.redirect(testURL[getRandomInt(2)], 302);
 // }
 
+
+
+
+
+class ElementHandler {
+	constructor(variantNumber){
+		this.variantNumber = variantNumber;
+	}
+	element(element) {
+	  	if (element.tagName == 'title'){
+	  		element.setInnerContent('Anthony Tsui');
+	  	}
+	  	if (element.tagName == 'h1'){
+	  		element.setInnerContent('Anthony Tsui Variant ' + (this.variantNumber+1))
+	  	}
+	  	if(element.tagName == 'p'){
+	  		element.setInnerContent('This is variant ' + (this.variantNumber==0 ? 'one' : 'two') + " of Anthony's take home project!")
+  	}
+  }
+
+  comments(comment) {
+    // An incoming comment
+  }
+
+  text(text) {
+    // An incoming piece of text
+  }
+
+  
+}
+
+
+
 async function handleRequest(request) {
 	try{
 		let response = await fetch(variantURL);
 		let data = await response.json();
 		let variantURLs = data["variants"];
-		console.log(variantURLs);
 
-		let returnURL = await fetch(variantURLs[getRandomInt(2)]);
-		let returnContent = await returnURL.text();
-		console.log(returnContent);
+		const res = await fetch(variantURLs[0]);
+
+		let variantNumber = getRandomInt(2)
+
+		let returnURL = await fetch(variantURLs[variantNumber]);//
+
+		console.log(variantNumber)
+
+		const rewriter = new HTMLRewriter()
+			.on('title', new ElementHandler(variantNumber))
+			.on('h1#title', new ElementHandler(variantNumber))
+			.on('p#description', new ElementHandler(variantNumber))
+			.on('a#url', new ElementHandler(variantNumber));
 
 
-		return new Response(returnContent, {
-		    headers: { 'content-type': 'text/html' },
-		  })
 
-
-
-		// return Response.redirect(variantURLs[getRandomInt(2)], 302);
+		return rewriter.transform(returnURL);
 	}
 	catch (err){
 		console.log('fetch failer', err)
 	}
 }
-
-
 
 function getRandomInt(max){
 	return Math.floor(Math.random() * Math.floor(max))
